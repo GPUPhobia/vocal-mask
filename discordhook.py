5dc8b574356f124cac553618bac0eb914ee607c4
esiu <siu.edward.w@gmail.com>
2019-04-25 16:02:30 -0700

    move auth to config file

--- discordhook.py
+++ discordhook.py
@@ -1,8 +1,12 @@
 import requests
 from discord import Webhook, RequestsWebhookAdapter
+import json
+
+with open("discord_config.json", "r") as inf:
+    discord_config = json.load(inf)
 
 def send_message(msg):
-    webhook_id = 571049429549842453
-    token = "3hWa6QVIMHBbryK2psgEcMl8QXx36X41Ryt279HPDtCI0mxMnoVSP8XmUwcYH-0shbaQ" 
+    webhook_id = discord_config["webhook_id"]
+    token = discord_config["token"]
     webhook = Webhook.partial(webhook_id, token, adapter=RequestsWebhookAdapter())
     webhook.send(msg, username="paperspace")
