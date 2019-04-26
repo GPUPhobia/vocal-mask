fix soft mask scaling

model.py
@@ -270,10 +270,11 @@ class Model(nn.Module):
         return Mvox, Macc
 
     def get_soft_mask(self, mask):
-        mask = mask/(np.max(mask))
         Mvox = mask * (mask > hp.eval_mask_threshold)
+        Mvox = Mvox/np.max(Mvox)
         inv_mask = 1 - mask
         Macc = inv_mask * (inv_mask > hp.eval_mask_threshold)
+        Macc = Macc/np.max(Macc)
         return Mvox, Macc
         
 