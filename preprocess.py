2a6b01cc538fc95ce763de1cd30cef7c0b5e9265
Edward Siu <siu.edward.w@gmail.com>
2019-04-29 19:35:38 -0400

    parallelize preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,67 +1,143 @@
 """
 Preprocess dataset
 
-usage: preprocess.py [options] <mix-dir> <vox-dir>
+usage: preprocess.py [options] <root-dir> <output-dir>
 
 options:
-     --output-dir=<dir>      Directory where processed outputs are saved. [default: data_dir].
+    --parser=<parser_type>  Type of dataset parser (dsd or musdb, default musdb)
+    --workers=<num-workers> Number of workers for multiprocessing (default 1)
     -h, --help              Show help message.
 """
-import os
-from docopt import docopt
-import numpy as np
-import math, pickle, os
+
 from audio import *
+import os
+import os.path
 from hparams import hparams as hp
-from utils import *
+from multiprocessing import Manager, Process
 from tqdm import tqdm
+import librosa
+import musdb
+from dsdparser import DSD
+import random
+import numpy as np
 import pickle
+from docopt import docopt
+
+# 2 args, the musdb root directory, and the directory to save the output
 
 
-def process_data(mix_dir, vox_dir, output_path, mix_path, vox_path): 
-    dataset_ids = []
-    mix_wavf = os.listdir(mix_dir)
-    vox_wavf = os.listdir(vox_dir)
-    count = len(mix_wavf)
-    for i in tqdm(range(count)):
-        file_id = f"{i:06d}"
+def load_musdb_sample(track):
+    audio = track.audio
+    sample_rate = 44100
+    #sample_rate = track.rate
+    if len(audio.shape) == 2:
+        audio = librosa.to_mono(audio.T)
+    if sample_rate != hp.sample_rate:
+        audio = librosa.resample(audio, sample_rate, hp.sample_rate)
+    return audio
 
-        # convert wav to spectrogram for mixture
-        mix_wav = load_wav(os.path.join(mix_dir, mix_wavf[i]))
-        mix_spec = spectrogram(mix_wav)[0]
-        mix_spec = mix_spec[np.newaxis,:,:] # single channel
-        
-        # convert wav to spectrogram for vocal
-        vox_wav = load_wav(os.path.join(vox_dir, vox_wavf[i]))
-        vox_spec = spectrogram(vox_wav)[0]
-        width = vox_spec.shape[1]
-        # we only want to predict the middle frame of vocal spectrogram
-        vox_spec = vox_spec[:,width//2]
+def load_dsd_sample(track):
+    return track.load()
 
-        # save output
-        dataset_ids.append(file_id)
-        np.save(os.path.join(mix_path,file_id+".npy"), mix_spec)
-        np.save(os.path.join(vox_path,file_id+".npy"), vox_spec)
-    
-    with open(os.path.join(output_path,'dataset_ids.pkl'), 'wb') as f:
-        pickle.dump(dataset_ids, f)
+def load_samples(track):
+    vocal_track = track.targets['vocals']
+    if parser_type == 'musdb':
+        mixture = load_musdb_sample(track)
+        vocal = load_musdb_sample(vocal_track)
+    else:
+        mixture = load_dsd_sample(track)
+        vocal = load_dsd_sample(vocal_track)
+    return mixture, vocal
 
+def generate_specs(L, track, idx, mixture_path, vocal_path):
+    mixture, vocal = load_samples(track)
+    mix_spec = spectrogram(mixture, power=hp.mix_power_factor)[0][np.newaxis,:,:]
+    vox_spec = spectrogram(vocal, power=hp.vox_power_factor)[0]
+    spec_id = f"spec{idx:06d}"
+    L.append((spec_id, mix_spec.shape[2]))
+    np.save(os.path.join(mixture_path, spec_id+".npy"), mix_spec)
+    np.save(os.path.join(vocal_path, spec_id+".npy"), vox_spec)
+
+def process_tracks(tracks, mix_path, vox_path, save_path):
+    with Manager() as manager:
+        spec_info = manager.list()
+        processes = []
+        for idx, track in enumerate(tqdm(tracks)):
+            if len(processes) >= num_workers:
+                for p in processes:
+                    p.join()
+                processes = []
+
+            p = Process(target=generate_specs, args=(spec_info, track, idx, mix_path, vox_path))
+            p.start()
+            processes.append(p)
+        if len(processes) > 0:
+            for p in processes:
+                p.join()
+        spec_info = list(spec_info)
+        with open(save_path, 'wb') as f:
+            random.shuffle(spec_info)
+            pickle.dump(spec_info, f)
 
 if __name__=="__main__":
     args = docopt(__doc__)
-    mix_dir = args["<mix-dir>"]
-    vox_dir = args["<vox-dir>"]
-    output_dir = args["--output-dir"]
+    root_dir = args["<root-dir>"]
+    output_dir = args["<output-dir>"]
+    parser_type = args["--parser"]
+    num_workers = args["--workers"]
+    if parser_type is None:
+        parser_type = 'musdb'
+    if num_workers is None:
+        num_workers = 1
+    else:
+        num_workers = int(num_workers)
 
-    # create paths
-    output_path = os.path.join(output_dir,"")
-    mix_path = os.path.join(output_path, "mix")
-    vox_path = os.path.join(output_path, "vox")
+    if parser_type == 'musdb':
+        dataset = musdb.DB(root_dir=root_dir, is_wav=True)
+        tracks = dataset.load_mus_tracks(subsets=['train'])
+        eval_tracks = dataset.load_mus_tracks(subsets=['test'])
+    else:
+        dataset = DSD(root_dir=root_dir)
+        tracks = dataset.load_tracks()
+        random.shuffle(tracks)
+        eval_tracks = tracks[:8]
+        tracks = tracks[8:]
 
-    # create dirs
-    os.makedirs(mix_path, exist_ok=True)
-    os.makedirs(vox_path, exist_ok=True)
+    #os.makedirs(output_dir, exist_ok=True)
+    mixture_path = os.path.join(output_dir, "mix")
+    vocal_path = os.path.join(output_dir, "vox")
+    eval_path = os.path.join(output_dir, "eval")
+    eval_mix_path = os.path.join(eval_path, "mix")
+    eval_vox_path = os.path.join(eval_path, "vox")
+    test_path = os.path.join(output_dir, "test")
+    test_mix_path = os.path.join(test_path, "mix")
+    test_vox_path = os.path.join(test_path,"vox")
+    os.makedirs(mixture_path, exist_ok=True)
+    os.makedirs(vocal_path, exist_ok=True)
+    #os.makedirs(eval_mix_path, exist_ok=True)
+    #os.makedirs(eval_vox_path, exist_ok=True)
+    #os.makedirs(test_mix_path, exist_ok=True)
+    #os.makedirs(test_vox_path, exist_ok=True)
 
-    # process data
-    process_data(mix_dir, vox_dir, output_path, mix_path, vox_path)
+    print("processing training examples")
+    train_info_path = os.path.join(output_dir, "spec_info.pkl")
+    process_tracks(tracks, mixture_path, vocal_path, train_info_path)
+    quit()
 
+    print("processing validation examples")
+    test_info_path = os.path.join(test_path, "test_spec_info.pkl")
+    process_tracks(tracks, test_mix_path, test_vox_path, test_info_path)
+
+    i = 0
+    print("slicing eval samples")
+    for track in tqdm(eval_tracks):
+        mixture, vocal = load_samples(track)
+        slices = get_wav_slices(mixture, hp.eval_length, hp.eval_length)
+        for j,k in slices:
+            # skip slices with no audio content
+            if np.sum(vocal[j:k]) == 0:
+                continue
+            fname = f"{i:06d}.wav"
+            save_wav(mixture[j:k], os.path.join(eval_mix_path, fname))
+            save_wav(vocal[j:k], os.path.join(eval_vox_path, fname))
+            i += 1
